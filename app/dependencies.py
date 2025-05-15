47512a2b4a39236cf63be18394ccf5e509672362
maladesov <m.zaharenkov@mail.ru>
2025-05-16 01:12:12 +0300

    ✨ S3 Port/adapter

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,8 +2,10 @@ from functools import lru_cache
 from fastapi import Depends
 import httpx
 
+from app.adapters.storage import S3StorageAdapter
 from app.adapters.teamservice import TeamServiceAdapter
 from app.adapters.userservice import UserServiceAdapter
+from app.ports.storage import IStoragePort
 from app.ports.teamservice import ITeamServicePort
 from app.ports.userservice import IUserServicePort
 
@@ -25,3 +27,8 @@ def get_user_service(
     client: httpx.AsyncClient = Depends(get_http_client),
 ) -> IUserServicePort:
     return UserServiceAdapter(client)
+
+
+@lru_cache
+def get_storage() -> IStoragePort:
+    return S3StorageAdapter()
