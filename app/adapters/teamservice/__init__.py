45a4e3c96ffa5aa4dc542cf05dd3a376ec26c05c
maladesov <m.zaharenkov@mail.ru>
2025-05-17 21:37:24 +0300

    ✨ 👽️ Новые фичи в TeamService

--- app/adapters/teamservice/__init__.py
+++ app/adapters/teamservice/__init__.py
@@ -2,6 +2,7 @@ from app.ports.teamservice.exceptions import TeamServiceError
 from app.ports.teamservice import ITeamServicePort
 from fastapi import HTTPException
 from app.config import Settings
+from typing import Any
 import urllib.parse
 import httpx
 
@@ -25,6 +26,23 @@ class TeamServiceAdapter(ITeamServicePort):
     async def _do_get(self, url: str) -> dict:
         try:
             response = await self.client.get(url, headers=self.headers)
+            data = response.json()
+            if response.status_code == 200:
+                return data
+            else:
+                raise HTTPException(
+                    status_code=response.status_code, detail=data["detail"]
+                )
+        except httpx.HTTPError as e:
+            raise TeamServiceError()
+
+    async def _do_post(self, url: str, json: Any) -> Any:
+        try:
+            response = await self.client.post(
+                url,
+                headers=self.headers,
+                json=json,
+            )
             data = response.json()
             if response.status_code == 200:
                 return data
@@ -60,3 +78,14 @@ class TeamServiceAdapter(ITeamServicePort):
             )
         )
         return HackathonTeamWithMatesDto(**data)
+
+    async def get_hackathon_team_info_many(
+        self, hackathon_id: int, team_ids: frozenset[int]
+    ) -> list[HackathonTeamDto]:
+        data: list[dict[str, Any]] = await self._do_post(
+            urllib.parse.urljoin(
+                self.base_url, f"hackathon/{hackathon_id}/teams-info-many"
+            ),
+            tuple(team_ids),
+        )
+        return [HackathonTeamDto(**team) for team in data]
--- app/ports/teamservice/__init__.py
+++ app/ports/teamservice/__init__.py
@@ -1,8 +1,10 @@
+from app.ports.teamservice.exceptions import TeamServiceError
+from collections import defaultdict
 from typing import Protocol
 
 from app.ports.teamservice.dto import (
-    HackathonTeamDto,
     HackathonTeamWithMatesDto,
+    HackathonTeamDto,
 )
 
 
@@ -11,15 +13,38 @@ class ITeamServicePort(Protocol):
     async def get_hackathon_teams(
         self, hackathon_id: int
     ) -> list[HackathonTeamDto]: ...
+    async def get_hackathon_team_info_many(
+        self, hackathon_id: int, team_ids: frozenset[int]
+    ) -> list[HackathonTeamDto]: ...
     async def get_hackathon_team(
         self, hackathon_id: int, team_id: int
     ) -> HackathonTeamWithMatesDto: ...
+
+    def get_hackathon_team_name_map(
+        self, teams: list[HackathonTeamDto]
+    ) -> defaultdict[int, str | None]:
+        name_map: defaultdict[int, str | None] = defaultdict(lambda: None)
+
+        for team in teams:
+            name_map[team.id] = team.name
+
+        return name_map
 
     async def try_get_team_info(self, team_id: int) -> HackathonTeamDto | None:
         try:
             return await self.get_team_info(team_id)
         except Exception:
             return None
+
+    async def try_get_hackathon_team_info_many(
+        self, hackathon_id: int, team_ids: frozenset[int]
+    ) -> list[HackathonTeamDto]:
+        try:
+            return await self.get_hackathon_team_info_many(
+                hackathon_id, team_ids
+            )
+        except TeamServiceError:
+            return []
 
     async def get_team_exists(self, team_id: int) -> bool:
         return await self.try_get_team_info(team_id) is not None
